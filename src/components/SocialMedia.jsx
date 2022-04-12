ab92f68e0b409dd1aae8b7343802bb1ad6896eb9
Ds3k <hakeemsek@gmail.com>
2022-04-12 16:28:56 -0500

    social icon update

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebook } from 'react-icons/fa'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
+
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
+            <a href='https://www.github.com/ds3k' target='_blank' rel='noreferrer'>
+              <div><BsGithub /></div>
+            </a>
             <a href='https://twitter.com/Ds3k_Inn' target='_blank' rel='noreferrer'>
               <div><BsTwitter /></div>
-            </a>
-            <a href='https://www.facebook.com/hakeem.seriki' target='_blank' rel='noreferrer'>
-              <div><FaFacebook /></div>
             </a>
             <a href='https://www.instagram.com' target='_blank' rel='noreferrer'>
               <div><BsInstagram /></div>
